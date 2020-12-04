4f4b63440be56a8ad434cb4ed493643a67122e11
Johan Smolders <johan.smolders@gmail.com>
2020-12-04 20:32:54 +0100

    Day 4

---
+++ src/main/kotlin/be/krikkrok/fifteen/AOC2015_4A.kt
@@ -0,0 +1,30 @@
+package be.krikkrok.fifteen
+
+import be.krikkrok.BaseAoc
+import java.security.MessageDigest
+import kotlin.system.exitProcess
+import kotlin.text.Charsets.UTF_8
+
+class AOC2015_4A: BaseAoc() {
+    init {
+        val input = "bgvyzdsv"
+
+        (0..10000000).forEach {
+            val currentCoin = "$input$it"
+            if(md5(currentCoin).toHex().startsWith("00000")){
+                println(md5(currentCoin).toHex())
+                println(it)
+                exitProcess(0)
+            }
+        }
+
+
+    }
+
+    fun md5(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(UTF_8))
+    fun ByteArray.toHex() = joinToString("") { "%02x".format(it) }
+}
+
+fun main() {
+    AOC2015_4A()
+}
